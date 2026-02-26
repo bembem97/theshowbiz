a04163c90c7110a5ef93f7e02207bb2b5b447565
bembem97 <bembemcabrera97@gmail.com>
2026-02-26 21:07:29 +0800

    fixed a minor bug in '/not-found'

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,6 +1,5 @@
-import { ButtonBack } from "@/components/custom/Button";
 import { Button } from "@/components/ui/button";
-import { ChevronLeftIcon, HomeIcon } from "lucide-react";
+import { HomeIcon } from "lucide-react";
 import Link from "next/link";
 
 export default async function NotFound() {
@@ -9,11 +8,11 @@ export default async function NotFound() {
       <h2>Not Found</h2>
       <p>Could not find requested resource</p>
       <div className="flex flex-wrap items-center *:text-sm">
-        <ButtonBack variant="link">
+        {/* <ButtonBack variant="link">
           <ChevronLeftIcon className="text-primary size-4" /> Go back to
           previous page
-        </ButtonBack>
-        <span>or return to</span>{" "}
+        </ButtonBack> */}
+        <span>Return to</span>{" "}
         <Button nativeButton={false} variant="link" render={<Link href="/" />}>
           <HomeIcon className="text-primary size-4" /> Home
         </Button>
